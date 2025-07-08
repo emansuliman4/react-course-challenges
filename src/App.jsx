99e691e375e5320c2c8f5c53480b6b81f7331019
emansuliman4 <emanmagdyahmed445@gmail.com>
2025-07-08 14:18:53 +0300

    Add reset  button to values of step and count

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { Steps } from "./components/steps";
 import { Counter } from "./components/counter";
+import { Button } from "./components/button";
 import { useState } from "react";
 
 function App() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
-
+  function Reset() {
+    setCount(0);
+    setStep(1);
+  }
   return (
     <div>
       <Steps step={step} setStep={setStep} />
       <Counter count={count} setCount={setCount} step={step} />
+      {count !== 0 || step !== 0 ? (
+        <Button label={"Reset"} onClick={Reset} />
+      ) : null}
     </div>
   );
 }
