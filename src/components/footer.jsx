c39330c3e5f6fa1febf6919abbe8bd914cf26eb0
emansuliman4 <emanmagdyahmed445@gmail.com>
2025-07-10 03:37:20 +0300

    remove old code

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,23 +1,3 @@
-// export default function Footer() {
-//   return (
-//     <footer>
-//       <em
-//         style={{
-//           width: "100%",
-//           backgroundColor: "#BEE9E8",
-//           display: "flex",
-//           alignItems: "center",
-//           justifyContent: "center",
-//           padding: "0",
-//           margin: "0",
-//           minHeight: "60px",
-//         }}
-//       >
-//         You have X tasks on your list, and you already packed X (x%)
-//       </em>
-//     </footer>
-//   );
-// }
 import "../styles/footer.css";
 export function Footer() {
   return (
